b5dda5785b433d159a5580ae1540e2374c121294
Fabian Greimel <fabgrei@gmail.com>
2020-02-19 16:25:49 +0100

    adaptions for notebook output

--- test/housing-simple.jl
+++ test/housing-simple.jl
@@ -1,7 +1,9 @@
+# # A model with homeowners and renters
+
 using Revise
 using Test, Aiyagari
 using QuantEcon, Parameters, Interpolations
-
+using Plots
 
 u(c; γ=γ) = c^(1-γ) / (1-γ)
 
@@ -24,9 +26,8 @@ mutable struct HousingAS{T1,T2,T3,T4} <: AggregateState
   dist::T4 # the distribution over idiosynchratic states
 end
 
-function HousingAS(r, p, a_grid, z_MC, param)
-  dist_proto = zeros((length(a_grid), length(z_MC.state_values)))
-  ρ = p * (param.δ + r)
+function HousingAS(r, p, a_grid, z_MC, param; ρ=p * (param.δ + r))
+  dist_proto = zeros((length(a_grid), length(z_MC.state_values))) 
   HousingAS(r, p, ρ, dist_proto)
 end
 
@@ -34,56 +35,91 @@ include("housing-simple-nlopt.jl")
 include("renting-nlopt.jl")
 #include("housing-simple-jump.jl")
 r = 0.29
- a_grid = LinRange(-√eps(), 5, 50)
+ a_grid = LinRange(-√eps(), 2.5, 50)
  param_own  = (β = 0.7, θ = 0.9, δ = 0.1, h_thres = 0.0)
  param_rent = (β = 0.7, θ = 0.9, δ = 0.1, h_thres = Inf)
  param_both = (β = 0.7, θ = 0.9, δ = 0.1, h_thres = 0.6)
- agg_state = HousingAS(r, 2.2, a_grid, z_MC, param)
+ agg_state = HousingAS(r, 2.2, a_grid, z_MC, param_both)
+
+# ## Forever home owners
 
 @unpack val, policy, policies_full = solve_bellman(a_grid, z_MC, agg_state, param_own, Owner())
-@unpack val, policy, policies_full = solve_bellman(a_grid, z_MC, agg_state, param_rent, Renter())
-@unpack val, policy, policies_full, owner = solve_bellman(a_grid, z_MC, agg_state, param_both, OwnOrRent())
- # 2.5 s with NLopt
- # 129 s 60 itr with JuMP
 
-plot(a_grid, owner)
+plot(a_grid, policies_full.h, title="house size", xlab="wealth", legend=:topleft)
+
+# 
+
+#plot(a_grid, policies_full.m, title="mortgage", xlab="wealth")
+
+dist = stationary_distribution(z_MC, a_grid, policies_full.w_next)
+plot(a_grid, dist, xlab="wealth" )
+
+# ## Forever renters
+
+a_grid_rent = LinRange(-√eps(), 5.0, 50)
+param_rent = (β = 0.7, θ = 0.9, δ = 0.1, h_thres = Inf)
+agg_state_rent = HousingAS(r, 2.2, a_grid_rent, z_MC, param_rent)
+
+@unpack val, policy, policies_full = solve_bellman(a_grid_rent, z_MC, agg_state_rent, param_rent, Renter())
+
+plot(a_grid_rent, policies_full.w_next, title="house size", xlab="wealth", legend=:topleft)
+
+# ### Stationary distribution
+
+dist = stationary_distribution(z_MC, a_grid_rent, policies_full.w_next)
+plot(a_grid_rent, dist, xlab="wealth" )
+
+# ## Own big, rent small
+
+@unpack val, policy, policies_full, owner = solve_bellman(a_grid, z_MC, agg_state, param_both, OwnOrRent())
 
 w_next_all = policies_full[1].w_next .* owner .+ policies_full[2].w_next .* .!owner
 h_all = policies_full[1].h .* owner .+ policies_full[2].h .* .!owner
+c_all = policies_full[1].c .* owner .+ policies_full[2].c .* .!owner
 
-scatter(a_grid, w_next_all)
-  plot!(a_grid, a_grid)
-  plot!(a_grid[[1;end]], a_grid[[end;end]], legend=false)
+plot(a_grid, owner, title="Who owns?")
 
-using Plots
-using DelimitedFiles
-#writedlm("test/matrices/housing_simple_nlopt_value.txt", val)
-value_test = readdlm("test/matrices/housing_simple_nlopt_value.txt")
+# 
 
-@test all(val .== value_test)
+# 
+# plot(a_grid, w_next_all, legend=:left, title="cash-at-hand")
+#   #plot!(a_grid, a_grid)
+#   #plot!(a_grid[[1;end]], a_grid[[end;end]], legend=false)
+# 
+# using Plots
+# using DelimitedFiles
+# #writedlm("test/matrices/housing_simple_nlopt_value.txt", val)
+# value_test = readdlm("test/matrices/housing_simple_nlopt_value.txt")
+# 
+# @test all(val .== value_test)
+# 
+# using Plots
+# plot(val)
+# 
+# scatter(a_grid, policies_full.w_next)
+# scatter(a_grid, policies_full[1].h)
+# scatter!(a_grid, policies_full[2].h)
 
-using Plots
-plot(val)
+plot(a_grid, h_all, legend=:topleft, title="House size")
+hline!([param_both.h_thres], color=:gray, label="", linestyle=:dash)
 
-scatter(a_grid, policies_full.w_next)
-scatter(a_grid, policies_full[1].h)
-scatter!(a_grid, policies_full[2].h)
-scatter(a_grid, h_all, legend=:topleft)
+# scatter(a_grid, policies_full[1].m .* owner)
+# scatter(a_grid, c_all)
+# 
+# all(policies_full.conv)
 
-scatter(a_grid, policies_full.m)
-scatter(a_grid, policies_full.c)
-
-all(policies_full.conv)
+# ## Stationary distribution 
 
 dist = stationary_distribution(z_MC, a_grid, w_next_all)
- plot(a_grid, dist)
-#writedlm("test/matrices/housing_simple_nlopt_dist.txt", dist)
-dist_test = readdlm("test/matrices/housing_simple_nlopt_dist.txt")
-@test all(dist .== dist_test)
+plot(a_grid, dist, xlab="wealth" )
 
-using StatsBase
-mean(vec(policies_full.m), Weights(vec(dist)))
-mean(vec(policies_full.h), Weights(vec(dist)))
-#926 μs
-
+# #writedlm("test/matrices/housing_simple_nlopt_dist.txt", dist)
+# dist_test = readdlm("test/matrices/housing_simple_nlopt_dist.txt")
+# @test all(dist .== dist_test)
+# 
+# using StatsBase
+# mean(vec(policies_full.m), Weights(vec(dist)))
+# mean(vec(policies_full.h), Weights(vec(dist)))
+# #926 μs
+# 
 
