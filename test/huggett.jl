4288a8a67f8f35f53562ba0589157fb56774bb99
Fabian Greimel <fabgrei@gmail.com>
2020-02-27 16:00:11 +0100

    update huggett test-file

--- test/huggett.jl
+++ test/huggett.jl
@@ -8,30 +8,48 @@ z_grid = [0.5; 1.0; 1.5]
 z_prob = [0.7 0.15 0.15;
           0.2 0.6 0.2;
           0.15 0.15 0.7]
-z_MC = MarkovChain(z_prob, z_grid)
+z_MC = MarkovChain(z_prob, z_grid, :z)
+
+exo = ExogenousStateSpace([z_MC])
+endo = EndogenousStateSpace((a=a_grid,))
 
 u(c) = c > 0 ? log(c) : 10000 * c - 10000 * one(c)
 
-function objective(choices, states, aggregate_state, 𝔼V, params)
+
+function get_c(choices, states, aggregate_state)
   @unpack a_next = choices
   @unpack a, z = states
   @unpack r = aggregate_state
-  @unpack β = params
   
   c = a + z - a_next/(1+r)
+end
+
+function objective(choices, states, aggregate_state, 𝔼V, params)
+  
+  @unpack a_next = choices
+  @unpack β = params
+  
+  c = get_c(choices, states, aggregate_state)
+
   u(c) + β * 𝔼V(a_next)
 end
 
-function Aiyagari.get_optimum(states, agg_state, 𝔼V, params, a_grid)
+
+function Aiyagari.get_optimum(states, agg_state, 𝔼V, params, endo, hh::Consumer)
+  a_grid = endo.grids.a
+  
   a_min, a_max = extrema(a_grid)
 
   res = optimize(a_next -> - objective((a_next=a_next,), states, agg_state, 𝔼V, params), a_min, a_max)
   
-  pol  = Optim.minimizer(res)
-  val  = - Optim.minimum(res)
-  conv = Optim.converged(res)
+  a_next = Optim.minimizer(res)
+  val    = - Optim.minimum(res)
+  conv   = Optim.converged(res)
   
-  (pol=pol, pol_full=missing, val=val, conv=conv)
+  pol = a_next
+  pol_full = (a_next=a_next, c=get_c((a_next=a_next, ), states, agg_state))
+  
+  (pol=pol, pol_full=pol_full, val=val, conv=conv)
 
 end
 
@@ -49,13 +67,17 @@ agg_state = HuggettAS(0.05, a_grid, z_MC)
 param = (β = 0.9, )
   
 #using BenchmarkTools
-@unpack value, policy = solve_bellman(a_grid, z_MC, agg_state, param)
+@unpack val, policy, policies_full = solve_bellman(endo, exo, agg_state, param, Consumer(), tol=√eps())
 # 22 ms 176 itr
 using DelimitedFiles
 #writedlm("test/matrices/huggett_value.txt", value)
 value_test = readdlm("test/matrices/huggett_value.txt")
 
-@test all(value .== value_test)
+using Plots
+plot(val)
+plot!(value_test)
+maximum(abs.(val .- value_test))
+@test all(val .≈ value_test)
 
 a_min, a_max = a_grid[[1;end]]
 @assert all(a_min .< policy .< a_max)
